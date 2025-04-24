aa0d1f9bf71e92ffd83ae29deface0c6f9a4c259
n5xt <1978@live.com>
2025-04-25 01:28:22 +0800

    feat: args from cli

--- index.js
+++ index.js
@@ -1,9 +1,48 @@
+import minimist from 'minimist'
 import { genUUID, genX25519 } from './utils.js'
 
-const uuid = genUUID()
-console.log('uuid', uuid)
+const gen = (options) => {
+  const { configName } = options || {}
+  const uuid = genUUID()
+  // console.log('uuid', uuid)
 
-const { publicKey, privateKey } = genX25519()
+  const { publicKey, privateKey } = genX25519()
 
-console.log('Public Key (Base64):', publicKey)
-console.log('Private Key (Base64):', privateKey)
+  // console.log('Public Key (Base64):', publicKey)
+  // console.log('Private Key (Base64):', privateKey)
+
+  // config
+  const cfg = {
+    "name": configName,
+    "settings": {
+      "clients": [
+        {
+          "id": uuid,
+        }
+      ],
+    },
+    "stream": {
+      "settings": {
+        "network": "tcp",
+        "security": {
+          publicKey,
+          privateKey
+        }
+      },
+    }
+  }
+
+  return cfg
+}
+
+(() => {
+  // cli args
+  const args = minimist(process.argv.slice(2))
+  const configName = args?.['name'] || 'default'
+
+  // generate
+  const config = gen({ configName })
+
+  // print result
+  console.log(JSON.stringify(config))
+})()
