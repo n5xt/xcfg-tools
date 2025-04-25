4d5fabd570a5cbc83faf1d300b01656ecf1d466f
n5xt <6938718+n5xt@users.noreply.github.com>
2025-04-25 06:43:21 +0000

    feat: json schema & validation

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import minimist from 'minimist'
+import Ajv from 'ajv'
 import { genUUID, genX25519 } from './utils.js'
+
+const ajv = new Ajv()
 
 const gen = (options) => {
   const { configName } = options || {}
@@ -13,29 +16,63 @@ const gen = (options) => {
 
   // config
   const cfg = {
-    "name": configName,
-    "settings": {
-      "clients": [
+    name: configName,
+    settings: {
+      clients: [
         {
-          "id": uuid,
+          id: uuid
         }
-      ],
+      ]
     },
-    "stream": {
-      "settings": {
-        "network": "tcp",
-        "security": {
+    stream: {
+      settings: {
+        network: 'tcp',
+        security: {
           publicKey,
           privateKey
         }
-      },
+      }
     }
   }
 
   return cfg
 }
 
-(() => {
+// config schema
+const schema = {
+  type: 'object',
+  properties: {
+    name: { type: 'string' },
+    settings: {
+      type: 'object',
+      properties: {
+        clients: { type: 'array' }
+      }
+    },
+    stream: {
+      type: 'object',
+      properties: {
+        settings: {
+          type: 'object',
+          properties: {
+            network: { type: 'string' },
+            security: {
+              type: 'object',
+              properties: {
+                publicKey: { type: 'string' },
+                privateKey: { type: 'string' }
+              }
+            }
+          }
+        }
+      }
+    }
+  },
+  required: ['name', 'settings', 'stream'],
+  additionalProperties: false
+}
+
+const run = () => {
   // cli args
   const args = minimist(process.argv.slice(2))
   const configName = args?.['name'] || 'default'
@@ -43,6 +80,15 @@ const gen = (options) => {
   // generate
   const config = gen({ configName })
 
-  // print result
-  console.log(JSON.stringify(config))
-})()
+  const validate = ajv.compile(schema)
+  const valid = validate(config)
+  if (!valid) {
+    // print errors
+    console.error(validate.errors)
+  } else {
+    // print result
+    console.log(JSON.stringify(config))
+  }
+}
+
+run()
